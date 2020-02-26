7138f74785b426e97bb89b1446d61c0ec7c965db
Christian León Pérez Serapio <cleonps@gmail.com>
2020-02-26 09:45:15 -0600

    Initial Commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by Christian León Pérez Serapio on 26/02/20.
+//  Copyright © 2020 Christian León Pérez Serapio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
