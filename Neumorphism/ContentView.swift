d3a935026173d38720c74522891250e25154740d
Christian León Pérez Serapio <cleonps@gmail.com>
2020-02-26 11:57:58 -0600

    Adds light button.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -8,9 +8,66 @@
 
 import SwiftUI
 
+extension Color {
+    static let offWhite = Color(red: 255/255, green: 255/255, blue: 235/255)
+    static let darkStart = Color(red: 50/255, green: 60/255, blue: 65/255)
+    static let darkEnd = Color(red: 25/255, green: 25/255, blue: 30/255)
+}
+
+extension LinearGradient {
+    init(_ colors: Color...) {
+        self.init(gradient: Gradient(colors: colors), startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .contentShape(Circle())
+        .background(
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .overlay(
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 4)
+                                .blur(radius: 4)
+                                .offset(x: 2, y: 2)
+                                .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
+                        )
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 8)
+                                .blur(radius: 4)
+                                .offset(x: -2, y: -2)
+                                .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                        )
+                } else {
+                    Circle()
+                    .fill(Color.offWhite)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.offWhite.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
+        )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.offWhite
+            Button(action: {
+                print("Button Tapped")
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+            }
+        .buttonStyle(SimpleButtonStyle())
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
